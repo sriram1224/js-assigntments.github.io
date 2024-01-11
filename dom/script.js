ce460d3266fbaef0d10b2b3f9e28265dbb7f7f34
sriram1224 <97927783+sriram1224@users.noreply.github.com>
2024-01-11 11:37:12 +0530

    Add files via upload

---
+++ dom/script.js
@@ -0,0 +1,11 @@
+var DivE  = document.getElementById("NewDiv");
+DivE.style.color = "blue";
+DivE.style.backgroundColor = "yellow";
+
+DivE.style.margin = "20px";
+DivE.style.padding = "10px";
+DivE.style.fontSize = "18px";
+DivE.style.fontWeight = "bold";
+
+DivE.style.height = "200px";
+DivE.style.width = "300px";
